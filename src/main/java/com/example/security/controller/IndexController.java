237b19aee47b8b60107b4e8600c6dc61ef3541ae
Owner <94shinshinhy@gmail.com>
2022-07-20 00:20:09 +0900

    SecurityConfig add

--- src/main/java/com/example/security/controller/IndexController.java
+++ src/main/java/com/example/security/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -11,6 +12,36 @@ public class IndexController {
         // 머스테치 기본 폴더 src/main/resources/
         // 뷰리졸브 : templates (prefix), .mustache (suffix) 생략가능
         return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody String login(){
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join(){
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "회원가입 완료됨";
     }
 
 }
