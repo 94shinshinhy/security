cc6929cc01cc366dc746d909b9584d44f9103ce9
Owner <94shinshinhy@gmail.com>
2022-07-25 23:39:50 +0900

    oauth

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -1,5 +1,12 @@
 package com.example.security.config;
 
+// 1. 코드받기(인증), 2. 엑세스토큰(권한)
+// 3. 사용자프로필을 가져오고 4-1. 그 정보를 토대로 회원가입을 자동으로 진행시키기도함
+// 4-2. (이메일, 전화번호, 이름, 아이디) 쇼핑몰 -> (집주소), 백화점몰 -> (vip등급, 일반등급)
+
+
+import com.example.security.oauth.PrincipalOauthUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,6 +19,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터 체인에 등록됨
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize, postAuthorize 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private PrincipalOauthUserService principalOauthUserService;
     @Bean // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
     public BCryptPasswordEncoder encodePwd(){
         return new BCryptPasswordEncoder();
@@ -29,6 +38,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login") // /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해줌
-                .defaultSuccessUrl("/");
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm")
+                .userInfoEndpoint()
+                .userService(principalOauthUserService); // 구글 로그인이 완료된 뒤의 후처리가 필요함. Tip. 코드x, (엑세스토큰+사용자프로필정보o)
     }
 }
